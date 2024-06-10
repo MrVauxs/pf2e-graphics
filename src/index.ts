dee810069d4a79b22bc64d00528db8ecb8457d6c
mrvauxs <mrvauxs@gmail.com>
2024-06-10 02:55:10 +0200

    chore

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
 Hooks.on("ready", () => {
     ui.notifications.info("PF2e API is ready!");
-
-    setTimeout(() => {
-        new Sequence()
-            .effect()
-                .file("jb2a.antilife_shell.blue_no_circle")
-                .atLocation(game.scenes.active.tokens.placeables[0])
-            .play()
-    }, 5000)
 })
