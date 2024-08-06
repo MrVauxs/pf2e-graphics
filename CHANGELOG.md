a4fae837810285dd1054c4b2cf8f117f96664442
MrVauxs <mrvauxs@gmail.com>
2024-08-06 14:18:17 +0200

    Working Custom Animations Menu on Actors
    * own solution
    
    * fix it all
    
    * IT WORKS!
    
    * [0.2.0]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.2.0] - 2024-08-06
+
+### Added
+
+- First version of the Actor Custom Animations (#3)
+
 ## [0.1.3] - 2024-08-02
 
 ### Fixed
@@ -110,7 +116,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Moved internal structure
 - Pre-release versions to be semver compliant
 
-[Unreleased]: https://github.com/MrVauxs/pf2e-graphics/compare/v0.1.3...HEAD
+[Unreleased]: https://github.com/MrVauxs/pf2e-graphics/compare/v0.2.0...HEAD
+[0.2.0]: https://github.com/MrVauxs/pf2e-graphics/compare/v0.1.3...v0.2.0
 [0.1.3]: https://github.com/MrVauxs/pf2e-graphics/compare/v0.1.2...v0.1.3
 [0.1.2]: https://github.com/MrVauxs/pf2e-graphics/compare/v0.1.1...v0.1.2
 [0.1.1]: https://github.com/MrVauxs/pf2e-graphics/compare/v0.1.0...v0.1.1
--- src/view/ActorAnimations/submenus/actor-animations.svelte
+++ src/view/ActorAnimations/submenus/actor-animations.svelte
@@ -1,86 +1,178 @@
 <script lang='ts'>
 	import type { TJSDocument } from '@typhonjs-fvtt/runtime/svelte/store/fvtt/document'
 	import { writableDerived } from '@typhonjs-fvtt/runtime/svelte/store/writable-derived'
-	import type { moduleFlags } from 'src/extensions'
-	import { AnimCore } from 'src/storage/AnimCore'
-	import { devMessage, log } from 'src/utils'
-	import AnimationCreator from 'src/view/AnimationCreator.svelte'
+	import { AnimCore, type JSONData } from 'src/storage/AnimCore'
+	import { camelToSpaces, devMessage, i18n, log } from 'src/utils'
 	import { getContext } from 'svelte'
 	import type { Writable } from 'svelte/store'
 
 	export let doc: TJSDocument<ActorPF2e>
 
-	// TODO: Fix this
 	const flag = writableDerived(
 		doc,
-		$doc => $doc.getFlag('pf2e-graphics', 'customAnimations'),
+		$doc => $doc.getFlag('pf2e-graphics', 'customAnimations') || {},
 		(data, doc) => {
-			doc.setFlag('pf2e-graphics', 'customAnimations', data)
+			function changeValue(obj: Record<string, any>) {
+				if (typeof obj === 'object') {
+					// iterating over the object using for..in
+					for (const key in obj) {
+						if (obj[key] === null) {
+							obj[`-=${key}`] = null
+							delete obj[key]
+						} else if (typeof obj[key] === 'object') {
+							changeValue(obj[key])
+						}
+					}
+				}
+				return obj
+			}
+
+			doc.setFlag('pf2e-graphics', 'customAnimations', changeValue(data))
 			return doc
 		},
-	) as Writable<moduleFlags>
-
-	$flag ??= {}
-	$flag.customAnimations ??= {}
+	) as Writable<Record<string, Exclude<JSONData[string], string>>>
 
 	const sessionStorage = getContext('#external').sessionStorage
 	const showNewAnimation = sessionStorage.getStore('showNewAnimation', false)
 	let newKey = ''
 
 	function createAnimation() {
-		if (!$flag?.customAnimations) throw log('Something went wrong, check with Vauxs!')
 		if (!newKey.length) throw ui.notifications.error('Primary roll option must not be empty!')
-		$flag.customAnimations[newKey] = []
+		$flag[newKey] = []
 		$showNewAnimation = !$showNewAnimation
 	}
 
-	function deleteAnimation(key: string) {
-		if (!$flag?.customAnimations) throw log('Something went wrong, check with Vauxs!')
-		delete $flag.customAnimations[key]
-		flag.set($flag)
+	async function deleteAnimation(key: string) {
+		Dialog.confirm({
+			content: `Are you sure you want to delete all animations under "${key}"?`,
+			title: 'Confirm Deletion',
+			yes: () => {
+				// @ts-ignore Explicitly a deletion thing
+				$flag[key] = null
+			},
+		})
 	}
 
-	$: devMessage('Actor Animations Tab', $flag.customAnimations, $showNewAnimation)
+	function bindJSON(event: Event, _property: any) {
+		_property = JSON.parse((event.target as HTMLInputElement).value)
+	}
+
+	$: devMessage('Actor Animations Tab', $flag, $showNewAnimation)
 </script>
 
 <div class='p-2 pb-0 flex flex-col h-full items-center'>
-	{#if $flag.customAnimations}
-		<div class='flex flex-col w-full items-center gap-1.5'>
-			{#each Object.keys($flag.customAnimations) as key}
-				<AnimationCreator
-					{key}
-					allAnimations={$flag.customAnimations}
-					deleteSelf={deleteAnimation}
+	<div class='flex flex-col w-full items-center gap-1.5'>
+		{#each Object.keys($flag).filter(x => Boolean($flag?.[x])) as key}
+			<div class='border border-solid bg-slate-300 bg-opacity-50 w-full rounded-sm p-2.5'>
+				<label class='flex items-center gap-1'>
+					<span>Primary Roll Option:</span>
+					<input type='text' value={key} disabled class='w-min text-center' />
+
+					<div class='ml-auto flex items-center'>
+						<button
+							on:click={() => { $flag[key] = [...$flag[key], AnimCore.CONST.TEMPLATE_ANIMATION()] }}
+							class='w-min text-nowrap items-center'
+						>
+							<i class='fa fa-plus' />
+							New Child Animation
+						</button>
+						<button
+							on:click={() => deleteAnimation(key)}
+							class='fa fa-trash-can size-8'
+						/>
+					</div>
+				</label>
+
+				{#if $flag[key].length}
+					<hr />
+					<div class='flex flex-col gap-1.5'>
+						{#each $flag[key] as animation}
+							<div class='p-1.5 border border-solid rounded-sm border-opacity-50 border-gray-800'>
+								<div class='flex gap-2 w-full'>
+									<button class='size-8 fa fa-chevron-down border-none' />
+
+									<label class='flex items-center gap-2'>
+										<span>Preset</span>
+										<select bind:value={animation.preset}>
+											{#each AnimCore.CONST.PRESETS as preset}
+												<option value={preset}>{camelToSpaces(preset).titleCase()}</option>
+											{/each}
+										</select>
+									</label>
+
+									<label class='flex items-center gap-2'>
+										<span>Trigger</span>
+										<select bind:value={animation.trigger}>
+											{#each AnimCore.CONST.TRIGGERS as trigger}
+												<option value={trigger}>{i18n(`triggers.${trigger}`)}</option>
+											{/each}
+										</select>
+									</label>
+
+									<label class='flex items-center gap-2'>
+										<span>Predicate</span>
+										<input
+											type='text'
+											value={JSON.stringify(animation.predicate || [])}
+											on:change={e => bindJSON(e, animation.predicate)}
+										/>
+									</label>
+
+									<div class='flex items-center gap-2 ml-auto text-nowrap'>
+										<button
+											on:click={() => Dialog.confirm({
+												content: `Are you sure you want to delete this animation?`,
+												title: 'Confirm Deletion',
+												yes: () => {
+													// @ts-ignore Explicitly a deletion thing
+													$flag[key] = $flag[key].filter(x => x !== animation)
+												},
+											})}
+											class='fa fa-trash-can size-8'
+										/>
+									</div>
+								</div>
+								<hr />
+								<div class='flex gap-2 w-full p-1'>
+									<label class='flex items-center gap-2'>
+										File
+										<!-- eslint-disable-next-line no-self-assign -->
+										<input type='text' bind:value={animation.file} on:change={() => animation = animation} />
+									</label>
+								</div>
+							</div>
+						{/each}
+					</div>
+				{/if}
+			</div>
+		{/each}
+	</div>
+	<div class='w-1/2 m-1 text-center items-center flex flex-col gap-1'>
+		{#if !$showNewAnimation}
+			<button on:click={() => $showNewAnimation = !$showNewAnimation}>
+				<i class='fas fa-plus' />
+				Create New Animation
+			</button>
+		{:else}
+			<span class='px-1'> Input the primary roll option. </span>
+			<div class='flex w-full gap-1'>
+				<input
+					type='text'
+					bind:value={newKey}
+					placeholder={Sequencer.Helpers.random_array_element(AnimCore.getKeys())}
 				/>
-			{/each}
-		</div>
-		<div class='w-1/2 m-1 text-center items-center flex flex-col gap-1'>
-			{#if !$showNewAnimation}
-				<button on:click={() => $showNewAnimation = !$showNewAnimation}>
-					<i class='fas fa-plus' />
-					Create New Animation
-				</button>
-			{:else}
-				<span class='px-1'> Input the primary roll option. </span>
-				<div class='flex w-full gap-1'>
-					<input
-						type='text'
-						bind:value={newKey}
-						placeholder={Sequencer.Helpers.random_array_element(AnimCore.getKeys())}
-					/>
-					<button
-						data-tooltip='pf2e-graphics.cancel'
-						class='fa fa-times w-min m-0'
-						on:click={() => $showNewAnimation = !$showNewAnimation} />
-				</div>
 				<button
-					class=''
-					on:click={createAnimation}
-				>
-					<i class='fas fa-check' />
-					Submit
-				</button>
-			{/if}
-		</div>
-	{/if}
+					data-tooltip='pf2e-graphics.cancel'
+					class='fa fa-times w-min m-0'
+					on:click={() => $showNewAnimation = !$showNewAnimation} />
+			</div>
+			<button
+				class=''
+				on:click={createAnimation}
+			>
+				<i class='fas fa-check' />
+				Submit
+			</button>
+		{/if}
+	</div>
 </div>
