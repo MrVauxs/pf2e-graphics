5b3f3aa4cd157a4cb9707055764f6ec297b58a80
mrvauxs <mrvauxs@gmail.com>
2024-08-07 22:52:14 +0200

    lint:fix

--- src/utils.ts
+++ src/utils.ts
@@ -62,15 +62,15 @@ export function camelToSpaces(string: string): string {
 
 export function clearEmpties(o: Record<string, any>) {
 	for (const k in o) {
-	  if (!o[k] || typeof o[k] !== 'object') {
+		if (!o[k] || typeof o[k] !== 'object') {
 			continue // If null or not an object, skip to the next iteration
-	  }
+		}
 
-	  // The property is an object
-	  clearEmpties(o[k]) // <-- Make a recursive call on the nested object
-	  if (Object.keys(o[k]).length === 0) {
+		// The property is an object
+		clearEmpties(o[k]) // <-- Make a recursive call on the nested object
+		if (Object.keys(o[k]).length === 0) {
 			delete o[k] // The object had no properties, so delete that property
-	  }
+		}
 	}
-	  return o
+	return o
 }
