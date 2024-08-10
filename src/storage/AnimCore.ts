99f67395a81480906f1ff655573a029b2f1c9565
mrvauxs <mrvauxs@gmail.com>
2024-08-10 20:20:44 +0200

    ignore a typing issue

--- src/storage/AnimCore.ts
+++ src/storage/AnimCore.ts
@@ -137,6 +137,7 @@ export let AnimCore = class AnimCore {
 		const userKeys = user.getFlag('pf2e-graphics', 'customAnimations') ?? {}
 		const actorKeys = actor?.getFlag('pf2e-graphics', 'customAnimations') ?? {}
 		const itemKeys = item?.getFlag('pf2e-graphics', 'customAnimations') ?? {}
+		// @ts-expect-error Fix in foundry-pf2e
 		const itemOriginKeys = item?.origin?.getFlag('pf2e-graphics', 'customAnimations') ?? {}
 
 		// Priority (highest to lowest): Item > Actor (Affected) > Actor (Origin) > User > Global
@@ -345,5 +346,6 @@ declare global {
 	interface pf2eGraphics {
 		modules: Record<string, Record<string, string | ReferenceObject | AnimationDataObject[]>>
 		AnimCore: typeof AnimCore
+		settings: typeof settings
 	}
 }
