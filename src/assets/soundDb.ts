3b2a9025cc120fa0515a6d621bf951305fdb7f6a
mrvauxs <mrvauxs@gmail.com>
2024-08-13 17:42:55 +0200

    ogg my odd

--- src/assets/soundDb.ts
+++ src/assets/soundDb.ts
@@ -4,67 +4,67 @@ const p = 'modules/pf2e-graphics/assets/library'
 const database = {
 	attack: {
 		miss: {
-			'01': `${p}/sounds/soniss/SWSH_Swing 3 Large 03_DDUMAIS_NONE.wav`,
+			'01': `${p}/sounds/soniss/SWSH_Swing 3 Large 03_DDUMAIS_NONE.ogg`,
 		},
 		sword: {
-			'01': `${p}/sounds/soniss/SWSH_Sword Slash Impact V2 Assorted 18_DDUMAIS_NONE.wav`,
+			'01': `${p}/sounds/soniss/SWSH_Sword Slash Impact V2 Assorted 18_DDUMAIS_NONE.ogg`,
 		},
 	},
 	lightning_bolt: {
 		impact: {
-			'01': `${p}/sounds/ovani-sounds/Magic/Shock/Lightning Bolt Impact A.wav`,
-			'02': `${p}/sounds/ovani-sounds/Magic/Shock/Lightning Bolt Impact B.wav`,
-			'03': `${p}/sounds/ovani-sounds/Magic/Shock/Lightning Bolt Impact C.wav`,
+			'01': `${p}/sounds/ovani-sounds/Magic/Shock/Lightning Bolt Impact A.ogg`,
+			'02': `${p}/sounds/ovani-sounds/Magic/Shock/Lightning Bolt Impact B.ogg`,
+			'03': `${p}/sounds/ovani-sounds/Magic/Shock/Lightning Bolt Impact C.ogg`,
 		},
 	},
 	bow: {
-		'01': `${p}/sounds/gamedev-market/BowWhistleShot1.wav`,
-		'02': `${p}/sounds/gamedev-market/BowWhistleShot2.wav`,
-		'03': `${p}/sounds/gamedev-market/BowWhistleShot3.wav`,
-		'04': `${p}/sounds/gamedev-market/BowWhistleShot4.wav`,
-		'05': `${p}/sounds/gamedev-market/BowWhistleShot5.wav`,
-		'06': `${p}/sounds/gamedev-market/BowWhistleShot6.wav`,
-		'07': `${p}/sounds/gamedev-market/BowWhistleShot7.wav`,
+		'01': `${p}/sounds/gamedev-market/BowWhistleShot1.ogg`,
+		'02': `${p}/sounds/gamedev-market/BowWhistleShot2.ogg`,
+		'03': `${p}/sounds/gamedev-market/BowWhistleShot3.ogg`,
+		'04': `${p}/sounds/gamedev-market/BowWhistleShot4.ogg`,
+		'05': `${p}/sounds/gamedev-market/BowWhistleShot5.ogg`,
+		'06': `${p}/sounds/gamedev-market/BowWhistleShot6.ogg`,
+		'07': `${p}/sounds/gamedev-market/BowWhistleShot7.ogg`,
 	},
 	crossbow: {
-		'01': `${p}/sounds/gamedev-market/Crossbow Shot A.wav`,
-		'02': `${p}/sounds/gamedev-market/Crossbow Shot B.wav`,
-		'03': `${p}/sounds/gamedev-market/Crossbow Shot C.wav`,
-		'04': `${p}/sounds/gamedev-market/Crossbow Shot D.wav`,
+		'01': `${p}/sounds/gamedev-market/Crossbow Shot A.ogg`,
+		'02': `${p}/sounds/gamedev-market/Crossbow Shot B.ogg`,
+		'03': `${p}/sounds/gamedev-market/Crossbow Shot C.ogg`,
+		'04': `${p}/sounds/gamedev-market/Crossbow Shot D.ogg`,
 	},
 	axe: {
 		slash: {
-			'01': `${p}/sounds/soundflakes/diablo-wrath-weapon/axe-hitting-metal_blood-splash.wav`,
+			'01': `${p}/sounds/soundflakes/diablo-wrath-weapon/axe-hitting-metal_blood-splash.ogg`,
 		},
 		throw: {
-			'01': `${p}/sounds/soundflakes/diablo-wrath-weapon/axe-throwing-hitting-flesh.wav`,
+			'01': `${p}/sounds/soundflakes/diablo-wrath-weapon/axe-throwing-hitting-flesh.ogg`,
 		},
 		miss: {
-			'01': `${p}/sounds/soundflakes/diablo-wrath-weapon/axe-throwing.wav`,
+			'01': `${p}/sounds/soundflakes/diablo-wrath-weapon/axe-throwing.ogg`,
 		},
 	},
 	flail: {
 		miss: {
-			'01': `${p}/sounds/soundflakes/diablo-wrath-weapon/bludgeon-throwing.wav`,
+			'01': `${p}/sounds/soundflakes/diablo-wrath-weapon/bludgeon-throwing.ogg`,
 		},
 	},
 	spear: {
 		hit: {
-			'01': `${p}/sounds/soundflakes/diablo-wrath-weapon/imperius-spear-hit-01.wav`,
-			'02': `${p}/sounds/soundflakes/diablo-wrath-weapon/imperius-spear-hit-02.wav`,
-			'03': `${p}/sounds/soundflakes/diablo-wrath-weapon/imperius-spear-hit-03.wav`,
-			'04': `${p}/sounds/soundflakes/diablo-wrath-weapon/imperius-spear-hit-04.wav`,
-			'05': `${p}/sounds/soundflakes/diablo-wrath-weapon/imperius-spear-hit-05.wav`,
+			'01': `${p}/sounds/soundflakes/diablo-wrath-weapon/imperius-spear-hit-01.ogg`,
+			'02': `${p}/sounds/soundflakes/diablo-wrath-weapon/imperius-spear-hit-02.ogg`,
+			'03': `${p}/sounds/soundflakes/diablo-wrath-weapon/imperius-spear-hit-03.ogg`,
+			'04': `${p}/sounds/soundflakes/diablo-wrath-weapon/imperius-spear-hit-04.ogg`,
+			'05': `${p}/sounds/soundflakes/diablo-wrath-weapon/imperius-spear-hit-05.ogg`,
 		},
 		miss: {
-			'01': `${p}/sounds/soundflakes/diablo-wrath-weapon/imperius-spear-whoosh.wav`,
+			'01': `${p}/sounds/soundflakes/diablo-wrath-weapon/imperius-spear-whoosh.ogg`,
 		},
 	},
 	critical: {
-		'01': `${p}/sounds/soundflakes/diablo-wrath-weapon/etherael-attack-01.wav`,
-		'02': `${p}/sounds/soundflakes/diablo-wrath-weapon/etherael-attack-02.wav`,
-		'03': `${p}/sounds/soundflakes/diablo-wrath-weapon/etherael-attack-03.wav`,
-		'04': `${p}/sounds/soundflakes/diablo-wrath-weapon/etherael-attack-04.wav`,
+		'01': `${p}/sounds/soundflakes/diablo-wrath-weapon/etherael-attack-01.ogg`,
+		'02': `${p}/sounds/soundflakes/diablo-wrath-weapon/etherael-attack-02.ogg`,
+		'03': `${p}/sounds/soundflakes/diablo-wrath-weapon/etherael-attack-03.ogg`,
+		'04': `${p}/sounds/soundflakes/diablo-wrath-weapon/etherael-attack-04.ogg`,
 	},
 
 	// Work in Progress
