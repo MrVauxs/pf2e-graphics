ab7dd8bee38a1e3599027e930c09a0a44850fd72
mrvauxs <mrvauxs@gmail.com>
2024-08-15 11:57:11 +0200

    say the source where an animation comes from

--- src/view/ItemAnimations/tabs/preset-animations.svelte
+++ src/view/ItemAnimations/tabs/preset-animations.svelte
@@ -1,5 +1,5 @@
 <script lang='ts'>
-	import { clearEmpties } from 'src/utils'
+	import { clearEmpties, i18n } from 'src/utils'
 
 	export let animData
 	export let item
@@ -17,13 +17,14 @@
 		<div class='ml-4'>
 			{#each Object.keys(animData.animations) as animationKey}
 				{#if animData.animations[animationKey].length > 0}
-					<h3>{animationKey} ({Object.keys(animData.sources)})</h3>
-					{JSON.stringify(animData.sources)}
+					<h3>
+						{animationKey} ({i18n(`source`, { format: Object.keys(animData.sources).at(-1) })})
+					</h3>
 					<div class='flex flex-col gap-1 ml-4'>
 						{#each animData.animations[animationKey] as animation}
 							<div class='flex flex-row w-[99%]'>
 								<textarea
-									class='flex-grow resize-y overflow-y-scroll'
+									class='flex-grow resize-y h-32 overflow-y-scroll'
 									disabled
 									value={JSON.stringify(clearEmpties(animation), null, ' ')}
 								/>
