fb3311f48ed38dccc72b5ecdd3e5c433543532c7
Spappz <34202141+Spappz@users.noreply.github.com>
2024-08-27 02:53:42 +0100

    tests

---
+++ tests/animations.ts
@@ -0,0 +1,72 @@
+// To test and report on all files, use `npm run test:animations`.
+// To check as quickly as possible whether any of the files are invalid, use `npm run test:animations -- fast`.
+// To test one specific file with issues listed, use `npm run test:animations -- <path to file>` (e.g. `npm run test:animation -- animations/actions/aid.json`).
+
+import * as fs from 'node:fs';
+import * as core from '@actions/core';
+import chalk from 'chalk';
+import { fromZodIssue } from 'zod-validation-error';
+import { validateAnimationJSON } from '../src/storage/animationsSchema';
+import { testFilesRecursively } from './helpers.ts';
+
+const targetPath = process.argv[2] && process.argv[2] !== 'fast' ? process.argv[2] : 'animations/';
+
+/* eslint-disable no-console */
+if (targetPath.endsWith('.json')) {
+	if (!fs.existsSync(targetPath)) throw new Error(`No such file exists at ${targetPath}`);
+	const json = JSON.parse(fs.readFileSync(targetPath, { encoding: 'utf8' }));
+	const result = validateAnimationJSON(json);
+	if (result.success) {
+		console.log(chalk.green('No validation errors!'));
+	} else {
+		console.log(
+			chalk.red(
+				`${chalk.bold(result.error.issues.length)} validation error${result.error.issues.length === 1 ? '' : 's'}:`,
+			),
+		);
+		result.error.issues.forEach((issue) => {
+			const formattedIssue = fromZodIssue(issue);
+			console.log(`  • ${formattedIssue.details[0].path.join('.')} :  ${formattedIssue.details[0].message}`);
+		});
+	}
+} else {
+	const results = testFilesRecursively(
+		targetPath,
+		{
+			'.json': (path) => {
+				if (path.match(/^[a-z0-9]+(-[a-z0-9]+)*$/))
+					return { success: false, message: 'Invalid filename.' };
+				const json = JSON.parse(fs.readFileSync(path, { encoding: 'utf8' }));
+				const result = validateAnimationJSON(json);
+				if (result.success) return { success: true };
+				return {
+					success: false,
+					message: `${chalk.bold(result.error.issues.length)} schema issue${result.error.issues.length === 1 ? '' : 's'}.`,
+				};
+			},
+			'default': false,
+		},
+		{ breakEarly: process.argv[2] === 'fast', ignoreGit: true },
+	);
+
+	const errors = results.filter(result => !result.success);
+	if (errors.length) {
+		core.startGroup(chalk.dim.underline('Invalid animation files:'));
+		const columnWidth = Math.min(Math.max(...errors.map(error => error.file.length + 3)), 60);
+		errors.forEach(result =>
+			core.info(
+				chalk.dim.bold(
+					`${result.file}${result.message ? `${' '.repeat(Math.max(columnWidth - result.file.length, 3))}${chalk.dim(result.message)}` : ''}`,
+				),
+			),
+		);
+		core.endGroup();
+		core.setFailed(
+			chalk.red(
+				`${chalk.bold(errors.length)} animation file${errors.length === 1 ? '' : 's'} contain schema-validation errors.`,
+			),
+		);
+	} else {
+		core.info(chalk.green('All animation files are valid!'));
+	}
+}
--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,3 +1,6 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+
 type Flatten<T extends object> = {
 	[K in keyof T]: T[K] extends object
 		? T[K] extends Array<any>
@@ -25,4 +28,79 @@ export function flatten<T extends object>(obj: T, parentKey: string = ''): Flatt
 	}
 
 	return result;
+}
+
+/**
+ * Gets a list of files by walking through the file-tree, starting from a particular path.
+ *
+ * @param targetPath - The initial path.
+ * @returns An array of file paths.
+ */
+export function getFilesRecursively(targetPath: string): string[] {
+	let fileList: string[] = [];
+	try {
+		fs.readdirSync(targetPath).forEach((file) => {
+			const filePath = path.join(targetPath, file);
+			if (fs.statSync(filePath).isDirectory()) {
+				fileList = fileList.concat(getFilesRecursively(filePath));
+			} else {
+				fileList.push(filePath);
+			}
+		});
+		return fileList;
+	} catch (e) {
+		if (fs.existsSync(targetPath)) return [targetPath];
+		throw e;
+	}
+}
+/**
+ * Walks through the file-tree, starting from a particular path, applying a test to each file.
+ *
+ * @param targetPath - The initial path. If the path is a directory, all files and subdirectories will be tested. If the path is a file, only that file will be tested.
+ * @param tests - An object mapping extensions (e.g. ".png", with the leading ".") to either a boolean or a test function. The behaviour for extensions without an explicit property is defined by the "default" property.
+ * @param options
+ * @param options.breakEarly - Stops testing upon the first error, returning all tested files up to and including it.
+ * @param options.ignoreGit - Ignores Git files, such as ".gitignore", ".gitkeep", etc. Requires `tests[""]` to be undefined.
+ * @returns An array of objects. The objects have a `file` property with the filepath, a `success` property with the success state, and optionally an `error` returned by the test function.
+ */
+export function testFilesRecursively(
+	targetPath: string,
+	tests: {
+		[key: string]: boolean | ((filepath: string) => { success: boolean; message?: string });
+	},
+	options?: { breakEarly?: boolean; ignoreGit?: boolean },
+): { file: string; success: boolean; message?: string }[] {
+	tests.default = tests.default ?? false;
+
+	options = options ?? {};
+	if (options.ignoreGit && tests[''] === undefined) {
+		tests[''] = (filepath) => {
+			const GIT_FILES = ['.gitignore', '.gitattributes', '.mailmap', '.gitmodules', '.gitkeep', '.keep'];
+			return {
+				success: GIT_FILES.includes(path.basename(filepath)),
+			};
+		};
+	}
+
+	const results = [];
+	for (const file of getFilesRecursively(targetPath)) {
+		const extension = path.extname(file);
+		const test = tests[extension] ?? tests.default;
+		if (typeof test === 'boolean') {
+			results.push({
+				file,
+				success: test,
+				message: test ? undefined : `Extension ${extension} is not allowed.`,
+			});
+			if (options.breakEarly && !test) return results;
+		} else {
+			const result = test(file);
+			results.push({
+				file,
+				...result,
+			});
+			if (options.breakEarly && !result.success) return results;
+		}
+	}
+	return results;
 }
