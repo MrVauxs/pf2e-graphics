61223de476c4aa7a8f9b39995d7aef93307ea941
Spappz <34202141+Spappz@users.noreply.github.com>
2024-08-31 21:00:40 +0100

    Revert "Merge branch 'shkeema' of https://github.com/Spappz/pf2e-graphics into shkeema"
    ~~vauxs already did it before i psuhed 😩~~
    
    i have no idea what's happening

--- src/view/VolumeControls/VolumeControls.svelte
+++ src/view/VolumeControls/VolumeControls.svelte
@@ -4,23 +4,14 @@
 	const volume = window.pf2eGraphics.storeSettings.getStore('volume');
 
 	if (!volume) throw new Error('There is no volume setting!?');
-
-	let element: HTMLElement;
-	let toggle = false;
-
-	$: if (toggle) game.tooltip.activate(element, { text: i18n('PLAYLIST.VOLUME.TOOLTIP', { volume: Math.round($volume * 100) }) });
 </script>
 
 <div class='pf2e-g'>
 	<li class='sound flexrow bg-purple-400/25 rounded-sm' data-tooltip='pf2e-graphics.animationsVolume-tooltip'>
-		<h4>{i18n('animationsVolume')}</h4>
+		<h4 class=''>{i18n('animationsVolume')}</h4>
 		<i class='volume-icon fas fa-volume-down'></i>
 		<input
-			bind:this={element}
 			bind:value={$volume}
-			on:mouseenter={() => toggle = true}
-			on:mouseout={() => toggle = false}
-			on:blur={() => toggle = false}
 			class='animations-volume-slider'
 			name='animationsVolume'
 			type='range' min='0' max='2' step='0.05'
