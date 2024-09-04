1737a26732b2d46d977c17e95628e8910f9313e8
Spappz <34202141+Spappz@users.noreply.github.com>
2024-09-04 06:03:26 +0100

    Update buildJSONSchema.ts

--- scripts/buildJSONSchema.ts
+++ scripts/buildJSONSchema.ts
@@ -8,14 +8,25 @@ import { Log } from './helpers';
 
 const OUTPUT_DIRECTORY = './dist';
 
-fs.writeFile(`${OUTPUT_DIRECTORY}/animations-schema.json`, JSON.stringify(getJSONSchema('animations')), {
-	encoding: 'utf8',
-})
-	.then(() => Log.info(p.green('Generated animations JSON schema.')))
-	.catch(() => Log.error(p.red('Failed to generate token-images JSON schema.')));
+fs.stat(OUTPUT_DIRECTORY)
+	.catch(() => {
+		Log.warning(`${OUTPUT_DIRECTORY} does not exist. Creating it...`);
+		fs.mkdir(OUTPUT_DIRECTORY);
+	})
+	.finally(() => {
+		fs.writeFile(`${OUTPUT_DIRECTORY}/animations-schema.json`, JSON.stringify(getJSONSchema('animations')), {
+			encoding: 'utf8',
+		})
+			.then(() => Log.info(p.green('Generated animations JSON schema.')))
+			.catch(() => Log.error(p.red('Failed to generate animations JSON schema.')));
 
-fs.writeFile(`${OUTPUT_DIRECTORY}/token-images-schema.json`, JSON.stringify(getJSONSchema('tokenImages')), {
-	encoding: 'utf8',
-})
-	.then(() => Log.info(p.green('Generated token-images JSON schema.')))
-	.catch(() => Log.error(p.red('Failed to generate token-images JSON schema.')));
+		fs.writeFile(
+			`${OUTPUT_DIRECTORY}/token-images-schema.json`,
+			JSON.stringify(getJSONSchema('tokenImages')),
+			{
+				encoding: 'utf8',
+			},
+		)
+			.then(() => Log.info(p.green('Generated token-images JSON schema.')))
+			.catch(() => Log.error(p.red('Failed to generate token-images JSON schema.')));
+	});
