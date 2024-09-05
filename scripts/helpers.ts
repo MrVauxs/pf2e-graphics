df25051248514d1cdd75cf1dedb00ca968842520
Spappz <34202141+Spappz@users.noreply.github.com>
2024-09-05 03:52:35 +0100

    idk this one is more dodgy but it's not used outside `src/` so i cba to overcomplicate

--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -221,5 +221,6 @@ export function testFilesRecursively(
 export function pluralise(word: string, count: number): string {
 	if (count === 1) return word;
 	if (word.endsWith('s')) return `${word}es`;
+	// if (word.endsWith('y')) return `${word.slice(0, -1)}ies`;
 	return `${word}s`;
 }
