62e4d79230e54b7191ecb852c9699241c0b8410b
Spappz <34202141+Spappz@users.noreply.github.com>
2024-09-16 03:10:16 +0100

    sigh

--- scripts/tests/animations.ts
+++ scripts/tests/animations.ts
@@ -24,6 +24,7 @@ if (!badFiles.length) {
 
 		if (!process.env.GITHUB_ACTIONS) return message;
 
+		console.log(file);
 		const JSONMap = parse(file);
 		const key = JSONMap.pointers[`/${issue.path.join('/')}`];
 		return {
