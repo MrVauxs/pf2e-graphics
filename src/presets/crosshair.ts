9632a587eb9250db1f766dfdd820fe613f26b442
mrvauxs <mrvauxs@gmail.com>
2024-09-27 10:11:21 +0200

    hell

--- src/presets/crosshair.ts
+++ src/presets/crosshair.ts
@@ -1,34 +1,40 @@
-import type { AnimationObject } from 'src/storage/animCore';
+import type { AnimationObject } from 'src/storage/AnimCore';
 import { ErrorMsg, log } from 'src/utils';
 import { type GameData, genericEffectOptions, parseOffsets, type SequencerTypes } from '.';
 
 export default function crosshair(seq: SequencerTypes, animation: AnimationObject, data: GameData) {
 	const { sources, targets = [] } = data;
 
+	seq
+		.managedCrosshairs()
+		.name('test');
+
 	return seq;
 }
 
 interface ManagedCrosshairsSection {}
 
-class ManagedCrosshairs extends Sequencer.BaseSection<ManagedCrosshairsSection> {
-	_name: string;
-
-	constructor(inSequence: Sequence) {
-		super(inSequence);
-		this._name = '';
-	}
-
-	name(inName: string) {
-		if (typeof inName !== 'string') throw new ErrorMsg('.name() - inName must be of type string');
-		this._name = inName;
-		return this;
-	}
-
-	async run() {
-		log(this._name);
-	}
-}
-
 Hooks.once('sequencerReady', () => {
+	class ManagedCrosshairs extends Sequencer.BaseSection<ManagedCrosshairsSection> {
+		_name: string;
+
+		constructor(inSequence: Sequence) {
+			super(inSequence);
+			this._name = '';
+		}
+
+		name(inName: string) {
+			if (typeof inName !== 'string') throw this.sequence._customError(this, 'name', 'inName must be of type string');
+			this._name = inName;
+			return this;
+		}
+
+		async run() {
+			await new Promise((resolve) => {
+				setTimeout(resolve, 5000);
+			});
+		}
+	}
+
 	Sequencer.SectionManager.registerSection('pf2e-graphics', 'managedCrosshairs', ManagedCrosshairs);
 });
