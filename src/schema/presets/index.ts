71c33da2267e12001cc11adb6eb224d85aab48e7
Spappz <34202141+Spappz@users.noreply.github.com>
2024-10-02 05:29:42 +0100

    add remainder of `graphicOptions`

--- src/schema/presets/index.ts
+++ src/schema/presets/index.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
-import { angle, filePath, hexColour, ID, sequencerDBEntry } from '../helpers/atoms';
-import { nonZero, uniqueItems } from '../helpers/refinements';
+import { angle, easing, filePath, hexColour, ID, sequencerDBEntry } from '../helpers/atoms';
+import { nonEmpty, nonZero, uniqueItems } from '../helpers/refinements';
 import { easingOptions, easingOptionsWithValue, offset, vector2 } from '../helpers/structures';
 
 /**
@@ -84,7 +84,9 @@ export const effectOptions = z
 			.optional(),
 	})
 	.strict()
-	.describe('Options which are common to all \'effect\' animation payloads (i.e. `sound`, `melee`, `ranged`, `onToken`, and `template`).');
+	.describe(
+		'Options which are common to all \'effect\' animation payloads (i.e. `sound`, `melee`, `ranged`, `onToken`, and `template`).',
+	);
 
 /**
  * Zod schema for the options which are common to all graphic animations.
@@ -152,6 +154,287 @@ export const graphicOptions = z
 			.strict()
 			.optional(),
 		targets: z.array(z.string()).min(1).optional(),
+		missed: z.literal(true).optional(),
+		anchor: vector2.optional(),
+		template: z
+			.object({
+				gridSize: z.number().positive(),
+				startPoint: z.number(),
+				endPoint: z.number(),
+			})
+			.strict()
+			.optional(),
+		wait: z
+			.number()
+			.or(
+				z
+					.object({
+						min: z.number(),
+						max: z.number(),
+					})
+					.strict()
+					.refine(obj => obj.max > obj.min, '`max` must be greater than `min`.'),
+			)
+			.optional(),
+		persist: z
+			.literal(true)
+			.or(
+				z
+					.object({
+						value: z.literal(true).optional(),
+						persistTokenPrototype: z.literal(true).optional(),
+					})
+					.strict()
+					.refine(...nonEmpty),
+			)
+			.optional(),
+		repeats: z
+			.number()
+			.min(1)
+			.int()
+			.or(
+				z
+					.object({
+						count: z.number().min(1),
+						delayMin: z.number().optional(),
+						delayMax: z.number().positive().optional(),
+					})
+					.strict()
+					.refine(
+						obj => (obj.delayMax ? obj.delayMin || obj.delayMin === 0 : true),
+						'`delayMin` is required if `delayMax` is defined.',
+					)
+					.refine(
+						obj => (obj.delayMax ? obj.delayMax > obj.delayMin! : true),
+						'`delayMax` must be greater than `delayMin`.',
+					),
+			)
+			.optional(),
+		loopProperty: z
+			.array(
+				z
+					.object({
+						target: z.string(),
+						property: z.string(),
+						options: z
+							.object({
+								duration: z.number(),
+								from: z.number().optional(),
+								to: z.number().optional(),
+								values: z
+									.array(z.number())
+									.min(1)
+									.refine(...uniqueItems)
+									.optional(),
+								loops: z.number().int().positive().optional(),
+								pingPong: z.literal(true).optional(),
+								delay: z.number().positive().optional(),
+								ease: easing.optional(),
+								fromEnd: z.literal(true).optional(),
+								gridUnits: z.literal(true).optional(),
+							})
+							.strict(),
+					})
+					.strict(),
+			)
+			.min(1)
+			.refine(...uniqueItems)
+			.optional(),
+		animateProperty: z
+			.array(
+				z
+					.object({
+						target: z.string(),
+						property: z.string(),
+						options: z
+							.object({
+								duration: z.number(),
+								from: z.number(),
+								to: z.number(),
+								delay: z.number().optional(),
+								ease: easing.optional(),
+								fromEnd: z.literal(true).optional(),
+								gridUnits: z.literal(true).optional(),
+							})
+							.strict(),
+					})
+					.strict(),
+			)
+			.min(1)
+			.refine(...uniqueItems)
+			.optional(),
+		shape: z
+			.array(
+				z
+					.object({
+						type: z.enum(['polygon', 'rectangle', 'circle', 'ellipse', 'roundedRect']),
+						radius: z.number().positive().optional(),
+						width: z.number().positive().optional(),
+						height: z.number().positive().optional(),
+						points: z
+							.array(z.tuple([z.number(), z.number()]).or(vector2))
+							.min(1)
+							.refine(...uniqueItems)
+							.optional(),
+						gridUnits: z.literal(true).optional(),
+						name: z.string().optional(),
+						fillColor: hexColour.or(z.number()).optional(),
+						fillAlpha: z.number().positive().optional(),
+						alpha: z.number().positive().optional(),
+						lineSize: z.number().positive().optional(),
+						lineColor: hexColour.or(z.number()).optional(),
+						offset: z
+							.object({
+								x: z.number().optional(),
+								y: z.number().optional(),
+								gridUnits: z.literal(true).optional(),
+							})
+							.strict()
+							.optional(),
+						isMask: z.literal(true).optional(),
+					})
+					.strict()
+					.refine(...nonEmpty),
+			)
+			.min(1)
+			.refine(...uniqueItems)
+			.optional(),
+		filter: z
+			.discriminatedUnion('type', [
+				z
+					.object({
+						type: z.literal('ColorMatrix'),
+						options: z
+							.object({
+								hue: angle
+									.refine(...nonZero)
+									.describe('The hue, in degrees.')
+									.optional(),
+								brightness: z
+									.number()
+									.describe('The value of the brightness (0 to 1, where 0 is black).')
+									.optional(),
+								contrast: z.number().describe('The value of the contrast (0 to 1).').optional(),
+								saturate: z
+									.number()
+									.describe(
+										'The value of the saturation amount. Negative numbers cause it to become desaturated (−1 to 1)',
+									)
+									.optional(),
+							})
+							.strict()
+							.refine(...nonEmpty),
+					})
+					.strict(),
+				z
+					.object({
+						type: z.literal('Glow'),
+						options: z
+							.object({
+								distance: z
+									.number()
+									.positive()
+									.describe('The distance of the glow, in pixels.')
+									.optional(),
+								outerStrength: z
+									.number()
+									.positive()
+									.describe('The strength of the glow outward from the edge of the sprite.')
+									.optional(),
+								innerStrength: z
+									.number()
+									.positive()
+									.describe('The strength of the glow inward from the edge of the sprite.')
+									.optional(),
+								color: hexColour.describe('The color of the glow').optional(),
+								quality: z
+									.number()
+									.gte(0)
+									.lte(1)
+									.describe(
+										'Describes the quality of the glow (0 to 1). A higher number is less performant.',
+									)
+									.optional(),
+								knockout: z
+									.literal(true)
+									.describe(
+										'Toggle to hide the contents and only show the glow (effectively hides the sprite).',
+									)
+									.optional(),
+							})
+							.strict()
+							.refine(...nonEmpty),
+					})
+					.strict(),
+				z
+					.object({
+						type: z.literal('Blur'),
+						options: z
+							.object({
+								strength: z.number().positive().describe('The strength of the filter.').optional(),
+								blur: z
+									.number()
+									.positive()
+									.describe(
+										'Sets the strength of the blur in both the horizontal and vertical axes simultaneously.',
+									)
+									.optional(),
+								blurX: z
+									.number()
+									.positive()
+									.describe('The strength of the blur on the horizontal axis.')
+									.optional(),
+								blurY: z
+									.number()
+									.positive()
+									.describe('The strength of the blur on the vertical axis.')
+									.optional(),
+								quality: z.number().int().positive().describe('Quality of the filter.').optional(),
+								resolution: z
+									.number()
+									.positive()
+									.describe('Sets the resolution of the blur filter.')
+									.optional(),
+								kernelSize: z
+									.number()
+									.positive()
+									.int()
+									.describe('Effectively how many passes the blur goes through.')
+									.optional(),
+							})
+							.strict()
+							.refine(...nonEmpty)
+							.refine(options => !options.blur || (!options.blurX && !options.blurY), {
+								path: ['blur'],
+								message: '`blur` cannot be used at the same time as `blurX` or `blurY`.',
+							}),
+					})
+					.strict(),
+				z
+					.object({
+						type: z.literal('Noise'),
+						options: z
+							.object({
+								noise: z.number().gt(0).lte(1).describe('The noise intensity.').optional(),
+								seed: z
+									.number()
+									.describe(
+										'A random seed for the noise generation (default is `Math.random()`).',
+									)
+									.optional(),
+							})
+							.strict()
+							.refine(...nonEmpty)
+							.optional(),
+					})
+					.strict(),
+				z
+					.object({
+						type: z.literal('Clip'),
+					})
+					.strict(),
+			])
+			.optional(),
 	})
 	.strict()
 	.describe('Options which are common to all graphic animations.');
