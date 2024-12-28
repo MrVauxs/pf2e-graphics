212d8936a7dc815a1662244eb7af15a71a802c00
Spappz <34202141+Spappz@users.noreply.github.com>
2024-12-28 03:08:41 +0000

    Improve integration with other modules providing animation sets
    - Foundry documentation technically states that `module.flags` must be an object (strictly `Record<string, unknown>`), so updated that. Also seems like a good idea if we ever add additional module flags anyway (string —> object is type-destructive).
    - Improve error-detection and -reporting in module-flags.
    
    No actual functionality changed.

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
-import { ErrorMsg } from '../utils.ts';
+import { error, i18n, info } from '../utils.ts';
 import { AnimCore } from './AnimCore.ts';
 
 Object.assign(window, {
@@ -10,17 +10,36 @@ Object.assign(window, {
 Hooks.once('ready', async () => {
 	for (const mod of game.modules) {
 		if (!mod.active) continue;
-		const animations: string | undefined = mod.flags?.['pf2e-graphics'] as unknown as string;
-		if (!animations) continue;
-		const path = animations.startsWith('modules/') ? animations : `modules/${mod.id}/${animations}`;
+
+		const moduleFlags = mod.flags['pf2e-graphics'];
+
+		if (!moduleFlags) continue;
+		if (typeof moduleFlags !== 'object' || !('path' in moduleFlags) || typeof moduleFlags.path !== 'string') {
+			error(
+				'pf2e-graphics.load.error.badModuleFlags',
+				{ title: mod.title, id: mod.id },
+				{ permanent: true },
+			);
+			continue;
+		}
+
+		const path = moduleFlags.path.startsWith('modules/')
+			? moduleFlags.path
+			: `modules/${mod.id}/${moduleFlags.path.replace(/^\.?[/\\]?/, '')}`;
 
 		try {
 			const resp = await fetch(path);
+			// HTML status code 200–299 = "OK :)"
+			if (String(resp.status)[0] !== '2')
+				throw new Error(i18n('pf2e-graphics.load.error.cantAccessFile', { path }));
 			const json = await resp.json();
 			window.pf2eGraphics.modules[mod.id] = json;
-		} catch {
-			// eslint-disable-next-line no-new
-			new ErrorMsg(`Failed to load ${mod.id} animations! Does the file exist?`);
+		} catch (err) {
+			error(
+				'pf2e-graphics.load.error.badModuleAnimations',
+				{ title: mod.title, id: mod.id },
+				{ permanent: true, cause: err },
+			);
 		}
 	}
 
@@ -28,31 +47,27 @@ Hooks.once('ready', async () => {
 
 	// Register the crosshair.ts sockets.
 	window.pf2eGraphics.socket = socketlib.registerModule('pf2e-graphics')!;
-	window.pf2eGraphics.socket.register(
-		'remoteLocation',
-		(name: string, location: object) =>
-			window.pf2eGraphics.locations.update((items) => {
-				items.push({ name, location });
-				return items;
-			}),
-	);
+	window.pf2eGraphics.socket.register('remoteLocation', (name: string, location: object) =>
+		window.pf2eGraphics.locations.update((items) => {
+			items.push({ name, location });
+			return items;
+		}));
 });
 
 if (import.meta.hot) {
 	import.meta.hot.on('updateAnims', (data) => {
 		window.pf2eGraphics.modules['pf2e-graphics'] = JSON.parse(data);
 		window.pf2eGraphics.AnimCore.updateAnimations();
-		ui.notifications.info('Animations updated!');
+		info('pf2e-graphics.load.notify.animationSetsUpdated');
 	});
 	import.meta.hot.on('updateValidationError', (data) => {
 		const array = JSON.parse(data);
-		ui.notifications.error(`${array.length} animation files failed validation! Check the console (F12) for details.`);
-		console.error('Validation errors: ', array);
+		error('pf2e-graphics.load.error.schemaValidationFailed', { number: array.length }, { data: array });
 	});
 	import.meta.hot.accept('./AnimationStorage.ts', (newModule) => {
 		if (newModule) {
 			window.pf2eGraphics.AnimCore = newModule?.AnimCore;
-			ui.notifications.info('AnimCore updated!');
+			info('pf2e-graphics.load.notify.AnimCoreUpdated');
 		}
 	});
 }
